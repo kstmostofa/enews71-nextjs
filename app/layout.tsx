5ea60dfcafef24a3aae78daf80c5c9e8820d2776
kstmostofa <mostofa122@gmail.com>
2025-02-25 01:15:16 +0600

    fix font issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 const kalpurush = localFont({
-  src: "/fonts/Kalpurush.ttf",
+  src: "./fonts/kalpurush.ttf",
   variable: "--font-sans",
 });
 
